2d3b4a74a2ad5ee212ad8a3a1c7f7b164dbe9b7c
Travis Staton <hello@travisstaton.com>
2012-12-25 22:57:48 -0500

    refactor provider into small methods

--- providers/default.rb
+++ providers/default.rb
@@ -20,33 +20,44 @@
 require 'pathname'
 
 action :create do
+  user = new_resource.user
+  home_dir = Pathname.new(Etc.getpwnam(user).dir).expand_path
   
+  install_packages
+  clone_oh_my_zsh(home_dir, user)
+  sym_link_zshrc(home_dir)
+  set_zsh_default(user)
+end
+
+def install_packages
   node['oh_my_zsh']['packages'].each do |pkg|
     package pkg
   end
+end
 
-  home_dir = Pathname.new(Etc.getpwnam(new_resource.user).dir).expand_path
-  
-  git "#{home_dir}/.oh-my-zsh" do
+def clone_oh_my_zsh(dir, user)
+    git "#{dir}/.oh-my-zsh" do
     repository "git://github.com/robbyrussell/oh-my-zsh.git"
     reference "master"
-    destination "#{home_dir}/.oh-my-zsh"
+    destination "#{dir}/.oh-my-zsh"
     action :sync
-    user "#{new_resource.user}"
+    user "#{user}"
   end
+end
 
-  # link .zshrc template unless a .zshrc already exists
-  link "#{home_dir}/.zshrc" do
-    to "#{home_dir}/.oh-my-zsh/templates/zshrc.zsh-template"
-    not_if "test -R #{home_dir}/.zshrc"
+def sym_link_zshrc(dir)
+  link "#{dir}/.zshrc" do
+    to "#{dir}/.oh-my-zsh/templates/zshrc.zsh-template"
+    not_if "test -R #{dir}/.zshrc"
   end
+end
 
-  # set default shell as zsh
-  execute "zsh-default" do
-    command "su -l -c 'chsh -s /bin/zsh #{new_resource.user}'"
+def set_zsh_default(user)
+  execute "set_zsh_as_default_for_#{user}" do
+    command "su -l -c 'chsh -s /bin/zsh #{user}'"
     cwd '/root'
     user "root"
-    not_if {Etc.getpwnam(new_resource.user).shell.include?("zsh")}
+    not_if {Etc.getpwnam(user).shell.include?("zsh")}
   end
-
 end
+
